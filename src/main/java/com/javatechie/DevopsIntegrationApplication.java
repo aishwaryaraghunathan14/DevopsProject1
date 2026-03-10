7720fc3da8dceebc952eb59afd940a6d392547b6
Sanjaykumar Patil <psanjayk04@gmail.com>
2026-03-10 14:12:39 +0530

    Update welcome message to include version V1

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "Welcome to our AWS/DvOps Project !!";
+		return "Welcome to our AWS/DvOps Project V1 !!";
 	}
 
 	public static void main(String[] args) {
